fc8a041b76662d16a16633d97aa2513321222fbf
jungini1123 <jungin070@gmail.com>
2026-09-20 01:18:06 +0900

    feat: JWT 발급/검증 컴포넌트 추가(#3)

--- src/main/java/com/hytodo/backend/HytodoBackendApplication.java
+++ src/main/java/com/hytodo/backend/HytodoBackendApplication.java
@@ -3,6 +3,7 @@ package com.hytodo.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @EnableJpaAuditing
 @SpringBootApplication
