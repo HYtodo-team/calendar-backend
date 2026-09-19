1b6b05f8c4d7a21073d40620d333cdcfdab7ee19
jungini1123 <jungin070@gmail.com>
2026-09-20 01:19:57 +0900

    fix: HytodoBackendApplication 어노테이션 추가(#3)

--- src/main/java/com/hytodo/backend/HytodoBackendApplication.java
+++ src/main/java/com/hytodo/backend/HytodoBackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @EnableJpaAuditing
 @SpringBootApplication
 public class HytodoBackendApplication {
